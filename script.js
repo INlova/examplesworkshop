d91bacc2e1e9a63a80e0310cc921ca9c3af47ff0
Michael A Nowiszewski <micski@gmail.com>
2017-12-06 01:23:42 +0000

    weather code to icn conv

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
 	navigator.geolocation.getCurrentPosition(function(position) { // location call  
 		var lat = position.coords.latitude;
 		var lon = position.coords.longitude;
-		$.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=8421ccdba1ed7d7315bbcdde0f687adf"
+		$.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=8421ccdba1ed7d7315bbcdde0f687adf", function(json) {
+
+			var icon = "<img src = 'http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
