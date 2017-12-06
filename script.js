ef6950b71c35c1c30baec53f0b6ce633ca82bf09
Michael A Nowiszewski <micski@gmail.com>
2017-12-06 00:43:54 +0000

    update 3 add location

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	navigator.geolocation.getCurrentPosition(function(position) { // location call  
+		var lat = position.coords.latitude;
+		var lon = position.coords.longitude;
+		$.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=8421ccdba1ed7d7315bbcdde0f687adf"
